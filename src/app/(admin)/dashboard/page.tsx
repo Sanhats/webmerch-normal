936cb416610d0d6049c1a649c25e955529150039
TomasDev <103080220+Sanhats@users.noreply.github.com>
2025-02-27 00:31:56 -0300

    Update page.tsx

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -17,7 +17,7 @@ interface Product {
   name: string;
   price: number;
   created_at: string;
-  category: Category | null;
+  category: Category | Category[] | null;
 }
 
 interface DashboardStats {
